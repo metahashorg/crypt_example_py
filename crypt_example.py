f97870fb887c4cabac8f709ee5f6af1dc3f996d6
KalinkinaMaria <mashyfycya@gmail.com>
2018-05-15 15:13:23 +0300

    Update script using cryptography module

--- crypt_example.py
+++ crypt_example.py
@@ -1,61 +1,75 @@
 #!/usr/bin/python3
 # coding: utf-8
 
-import os
+import binascii
 import hashlib
-import subprocess
+try:
+    from cryptography.hazmat.backends import default_backend
+    from cryptography.hazmat.primitives.asymmetric import ec
+    from cryptography.hazmat.primitives.serialization import Encoding, PrivateFormat, NoEncryption, PublicFormat
+except ImportError:
+    print("Something went wrong, check your cryptography module installation")
 
-FNULL = open(os.devnull, 'w')
+if __name__ == '__main__':
+    print("Step 1. Generate rpivate and public keys. Take part of the public "
+          "key that equals to 65 bytes.")
+    key = ec.generate_private_key(ec.SECP256K1(), default_backend())
 
-if __name__ == "__main__":
-    print("Generating private key")
+    key_pem = key.private_bytes(encoding=Encoding.PEM,
+                                format=PrivateFormat.TraditionalOpenSSL,
+                                encryption_algorithm=NoEncryption())
 
-    result = subprocess.call("openssl ecparam -genkey -name secp256k1 -out mh.pem",
-                             shell=True, stdout=FNULL, stderr=subprocess.STDOUT)
+    pub_key = key.public_key()
+    pub_key_der = pub_key.public_bytes(encoding=Encoding.DER,
+                                format=PublicFormat.SubjectPublicKeyInfo)
+    pub_key_pem = pub_key.public_bytes(encoding=Encoding.PEM,
+                                format=PublicFormat.SubjectPublicKeyInfo)
 
-    if result == 0:
-        print("Done")
-    else:
-        print("Something went wrong, check your openssl installation")
-        exit()
+    pub_key_der_65 = pub_key_der[:65]
+    pub_key = binascii.b2a_hex(pub_key_der_65)
+    print("Done")
 
-    print("Generating public key")
-    result = subprocess.call("openssl ec -in mh.pem -pubout -outform DER|tail -c 65|xxd -p -c 65 > mh_addr.pub",
-                             shell=True, stdout=FNULL, stderr=subprocess.STDOUT)
+    print("Step 2. Perform SHA-256 hash on the public key.")
+    h = hashlib.new('sha256')
+    t = binascii.a2b_hex(pub_key)
+    h.update(binascii.a2b_hex(pub_key))
+    resulrt_sha256 = h.hexdigest()
+    print("Done")
 
-    if result == 0:
-        print("Done")
-    else:
-        print("Something went wrong, check your openssl installation")
-        exit()
+    print("Step 3. Perform RIPEMD-160 hash on the result of previous step.")
+    h = hashlib.new('rmd160')
+    h.update(binascii.a2b_hex(resulrt_sha256.encode('utf-8')))
+    resulrt_rmd160 = '00' + h.hexdigest()
+    print("Done")
 
-    print("Generating Metahash Address")
+    print("Step 4. SHA-256 hash is calculated on the result of previous step.")
+    h = hashlib.new('sha256')
+    h.update(binascii.a2b_hex(resulrt_rmd160.encode('utf-8')))
+    resulrt_sha256rmd = h.hexdigest()
+    print("Done")
 
-    with open("mh_addr.pub", 'br') as f:
-        pub_key = f.read()
+    print("Step 5. Another SHA-256 hash performed on value from Step 4 and "
+          "save first 4 bytes.")
+    h = hashlib.new('sha256')
+    h.update(binascii.a2b_hex(resulrt_sha256rmd.encode('utf-8')))
+    resulrt_sha256rmd_again = h.hexdigest()
+    first4_resulrt_sha256rmd_again = resulrt_sha256rmd_again[:8]
+    print("Done")
 
-        h = hashlib.new('sha256')
-        h.update(pub_key)
-        resulrt_sha256 = h.hexdigest()
+    print("Step 6. These 4 bytes from last step added to RIPEMD-160 hash with "
+          "prefix 0x. ")
+    address = '0x' + resulrt_rmd160 + first4_resulrt_sha256rmd_again
 
-        h = hashlib.new('rmd160')
-        h.update(resulrt_sha256.encode('utf-8'))
-        resulrt_rmd160 = '00' + h.hexdigest()
+    print("Your Metahash address is %s" % address)
 
-        h = hashlib.new('sha256')
-        h.update(resulrt_rmd160.encode('utf-8'))
-        resulrt_sha256rmd = h.hexdigest()
+    text_file = open("mh_address.txt", "w")
+    text_file.write(address)
+    text_file.close()
 
-        h = hashlib.new('sha256')
-        h.update(resulrt_sha256rmd.encode('utf-8'))
-        resulrt_sha256rmd_again = h.hexdigest()
+    text_file = open("mh_public.pub", "w")
+    text_file.write(pub_key.decode("utf-8"))
+    text_file.close()
 
-        first4_resulrt_sha256rmd_again = resulrt_sha256rmd_again[:4]
-
-        address = '0x' + resulrt_rmd160 + first4_resulrt_sha256rmd_again
-
-        print("Your Metahash address is %s" % address)
-
-        text_file = open("mh_address.txt", "w")
-        text_file.write(address)
-        text_file.close()
+    text_file = open("mh_private.pem", "w")
+    text_file.write(key_pem.decode("utf-8"))
+    text_file.close()
