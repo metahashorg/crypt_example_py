b36f5ec1ae166b416e81944f867b10f8816a15a7
KalinkinaMaria <mashyfycya@gmail.com>
2018-05-16 10:26:00 +0300

    Update: add subsidiary functions

--- crypt_example.py
+++ crypt_example.py
@@ -16,8 +16,32 @@ except ImportError:
 def create_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument('-g', '--generate', action='store_true',
-                        help='generate MH address')
+                        help='generate MH address to mh_address.txt')
     return parser
+
+
+def save_to_file(text, file_name):
+    with open(file_name, 'w') as f:
+        f.write(text)
+
+
+def hex_point_coordinate(coordinate_value):
+    value_hex = hex(coordinate_value)
+    value_hex = value_hex[2:]
+    value_len = 64 - len(value_hex)
+    return value_hex if value_len <= 0 else '0' * value_len + value_hex
+
+
+def get_code(pub_key):
+    x_hex = hex_point_coordinate(pub_key.public_numbers().x)
+    y_hex = hex_point_coordinate(pub_key.public_numbers().y)
+    return '04' + str(x_hex) + str(y_hex)
+
+
+def hash_code(code, algth):
+    h = hashlib.new(algth)
+    h.update(binascii.a2b_hex(code))
+    return h.hexdigest()
 
 
 def generate_metahash_address():
@@ -28,72 +52,42 @@ def generate_metahash_address():
     private_key_pem = private_key.private_bytes(encoding=Encoding.PEM,
                                 format=PrivateFormat.TraditionalOpenSSL,
                                 encryption_algorithm=NoEncryption())
+    save_to_file(private_key_pem.decode("utf-8"), "mh_private.pem")
 
     pub_key = private_key.public_key()
     pub_key_der = pub_key.public_bytes(encoding=Encoding.DER,
                                        format=PublicFormat.SubjectPublicKeyInfo)
     pub_key_pem = binascii.b2a_hex(pub_key_der)
-    pub_key_pem1 = pub_key.public_bytes(encoding=Encoding.PEM,
-                                       format=PublicFormat.SubjectPublicKeyInfo)
+    save_to_file(pub_key_pem.decode("utf-8"), "mh_public.pub")
 
-    x = pub_key.public_numbers().x
-    x_hex = hex(x)
-    x_hex = x_hex[2:]
-    x_len = 64 - len(x_hex)
-    x_hex = x_hex if x_len <= 0 else '0' * x_len + x_hex
-    y = pub_key.public_numbers().y
-    y_hex = hex(y)
-    y_hex = y_hex[2:]
-    y_len = 64 - len(y_hex)
-    y_hex = y_hex if y_len <= 0 else '0' * y_len + y_hex
-
-
-    code = '04' + str(x_hex) + str(y_hex)
+    code = get_code(pub_key)
     print("Done")
 
     print("Step 2. Perform SHA-256 hash on the public key.")
-    h = hashlib.new('sha256')
-    h.update(binascii.a2b_hex(code))
-    resulrt_sha256 = h.hexdigest()
+    resulrt_sha256 = hash_code(code, 'sha256')
     print("Done")
 
     print("Step 3. Perform RIPEMD-160 hash on the result of previous step.")
-    h = hashlib.new('rmd160')
-    h.update(binascii.a2b_hex(resulrt_sha256.encode('utf-8')))
-    resulrt_rmd160 = '00' + h.hexdigest()
+    resulrt_rmd160 = '00' + hash_code(resulrt_sha256.encode('utf-8'), 'rmd160')
     print("Done")
 
     print("Step 4. SHA-256 hash is calculated on the result of previous step.")
-    h = hashlib.new('sha256')
-    h.update(binascii.a2b_hex(resulrt_rmd160.encode('utf-8')))
-    resulrt_sha256rmd = h.hexdigest()
+    resulrt_sha256rmd = hash_code(resulrt_rmd160.encode('utf-8'), 'sha256')
     print("Done")
 
     print("Step 5. Another SHA-256 hash performed on value from Step 4 and "
           "save first 4 bytes.")
-    h = hashlib.new('sha256')
-    h.update(binascii.a2b_hex(resulrt_sha256rmd.encode('utf-8')))
-    resulrt_sha256rmd_again = h.hexdigest()
+    resulrt_sha256rmd_again = hash_code(resulrt_sha256rmd.encode('utf-8'),
+                                        'sha256')
     first4_resulrt_sha256rmd_again = resulrt_sha256rmd_again[:8]
     print("Done")
 
     print("Step 6. These 4 bytes from last step added to RIPEMD-160 hash with "
           "prefix 0x. ")
     address = '0x' + resulrt_rmd160 + first4_resulrt_sha256rmd_again
+    save_to_file(address, "mh_address.txt")
 
     print("Your Metahash address is %s" % address)
-
-    text_file = open("mh_address.txt", "w")
-    text_file.write(address)
-    text_file.close()
-
-    text_file = open("mh_public.pub", "w")
-    text_file.write(pub_key_pem.decode("utf-8"))
-    text_file.close()
-
-    text_file = open("mh_private.pem", "w")
-    text_file.write(private_key_pem.decode("utf-8"))
-    text_file.close()
 
 
 if __name__ == '__main__':
@@ -105,5 +99,3 @@ if __name__ == '__main__':
         generate_metahash_address()
     else:
         arg_parser.print_help()
-
-
